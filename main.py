677eb471f0fc637065f34ff135f55ce3449b4078
BreezeDawn <BreezeGent@gmail.com>
2018-08-28 15:42:12 +0800

    menager脚本启动

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from datetime import timedelta
 
-from flask import Flask,session
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_session import Session
-
+from flask_script import Manager
 
 class Config:
     # app.config
@@ -23,7 +23,8 @@ class Config:
     SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # session存储使用的redis对象,默认是Redis(),我们要使用StrictRedis
     SESSION_USE_SIGNER = True  # 是否对sessionID进行加密存储
     SECRET_KEY = '21cG0Z3SmdeqmPFn1gykFaTC+uSgBRx6LtmKJg6lFKAFH33zgSO9tpfE6Wtvnzvi'  # base64.b64encode(os.urandom(48))
-    PERMANENT_SESSION_LIFETIME = timedelta(days=7) # session持久化存储时间,默认持久化存储31days
+    PERMANENT_SESSION_LIFETIME = timedelta(days=7)  # session持久化存储时间,默认持久化存储31days
+
 
 app = Flask(__name__)
 
@@ -35,13 +36,15 @@ sr = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 
 Session(app)
 
+# 脚本启动
+manager = Manager(app)
 
 @app.route('/')
 def index():
     sr.set('name', 'maimai')  # redis存储测试
-    session['name'] = 'maimai' # session存储测试,现在会存储到redis
+    session['name'] = 'maimai'  # session存储测试,现在会存储到redis
     return 'hello'
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
