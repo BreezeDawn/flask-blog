0784df8921ec8601d951ce9d8a8ddecb7678ebc6
BreezeDawn <BreezeGent@gmail.com>
2018-08-28 15:59:37 +0800

    创建Config.py,封装配置类并生成不同环境的配置

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from datetime import timedelta
-
 from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
@@ -7,26 +5,7 @@ from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-
-class Config:
-    # app.config
-    DEBUG = True
-
-    # sqlalchemy.config
-    SQLALCHEMY_DATABASE_URI = "mysql://root:xiaodianchi@127.0.0.1:3306/flask_blog"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # 是否追踪数据库变化
-
-    # redis.config
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = '6379'
-
-    # session.config
-    SESSION_TYPE = 'redis'  # session存储的数据库类型
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # session存储使用的redis对象,默认是Redis(),我们要使用StrictRedis
-    SESSION_USE_SIGNER = True  # 是否对sessionID进行加密存储
-    SECRET_KEY = '21cG0Z3SmdeqmPFn1gykFaTC+uSgBRx6LtmKJg6lFKAFH33zgSO9tpfE6Wtvnzvi'  # base64.b64encode(os.urandom(48))
-    PERMANENT_SESSION_LIFETIME = timedelta(days=7)  # session持久化存储时间,默认持久化存储31days
-
+from config import Config
 
 app = Flask(__name__)
 
