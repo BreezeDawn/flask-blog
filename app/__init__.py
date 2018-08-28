97de80a24958bd9f2f872214e18a4100463951d6
BreezeDawn <BreezeGent@gmail.com>
2018-08-28 18:55:22 +0800

    为app.__init__.py文件增加注释

--- app/__init__.py
+++ app/__init__.py
@@ -6,27 +6,33 @@ from flask_sqlalchemy import SQLAlchemy
 
 from config import config_dict
 
-
+# 数据库全局变量
 db = None   # type: SQLAlchemy
 sr = None   # type: StrictRedis
 
 
 def create_app(config_type):
+    # 初始化app对象
     app = Flask(__name__)
 
+    # 获取传入环境对应的配置类,并进行配置设定
     config_class = config_dict[config_type]
     app.config.from_object(config_class)
 
+    # 数据库全局设定
     global db, sr
 
     db = SQLAlchemy(app)
 
     sr = StrictRedis(host=config_class.REDIS_HOST, port=config_class.REDIS_PORT)
 
+    # Session设定
     Session(app)
 
-    Migrate(app, db)  # 初始化迁移器
+    # 初始化迁移器
+    Migrate(app, db)
 
+    # 创建主蓝图
     from app.main import main_blu
     app.register_blueprint(main_blu)
 
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,5 @@
-from app import sr
 from . import main_blu
-
 
 @main_blu.route('/')
 def index():
-    sr.set('name', 'mai')  # 测试sr使用
     return 'hello'
