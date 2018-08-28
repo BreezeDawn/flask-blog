4226dedec3ab2adff0926c72b91945546b942cd7
BreezeDawn <BreezeGent@gmail.com>
2018-08-28 14:55:11 +0800

    连接数据库并添加数据库配置

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 class Config:
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "mysql://root:xiaodianchi@127.0.0.1:3306/flask_blog"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # 是否追踪数据库变化
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
     return 'hello'
+
 
 if __name__ == '__main__':
     app.run()
