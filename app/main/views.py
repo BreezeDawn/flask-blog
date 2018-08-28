be374e97ecf5c58b47a825af0b706778d9c71dc0
BreezeDawn <BreezeGent@gmail.com>
2018-08-28 21:49:07 +0800

    增加favicon.ico文件和对应路由

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,13 @@
+from flask import send_from_directory
+
 from . import main_blu
+
 
 @main_blu.route('/')
 def index():
     return 'hello'
+
+
+@main_blu.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static/image', 'favicon.ico')
