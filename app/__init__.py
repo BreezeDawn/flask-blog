eaf3c581a1011fc34e361c7d7ff973ad59d0acb5
BreezeDawn <BreezeGent@gmail.com>
2018-08-28 18:37:52 +0800

    创建数据库全局对象,优化蓝图导入位置

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,11 @@ from flask_session import Session
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-from app.main import main_blu
 from config import config_dict
+
+
+db = None   # type: SQLAlchemy
+sr = None   # type: StrictRedis
 
 
 def create_app(config_type):
@@ -14,14 +17,17 @@ def create_app(config_type):
     config_class = config_dict[config_type]
     app.config.from_object(config_class)
 
+    global db, sr
+
     db = SQLAlchemy(app)
 
-    StrictRedis(host=config_class.REDIS_HOST, port=config_class.REDIS_PORT)
+    sr = StrictRedis(host=config_class.REDIS_HOST, port=config_class.REDIS_PORT)
 
     Session(app)
 
     Migrate(app, db)  # 初始化迁移器
 
+    from app.main import main_blu
     app.register_blueprint(main_blu)
 
     return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
+from app import sr
 from . import main_blu
 
 
 @main_blu.route('/')
 def index():
+    sr.set('name', 'mai')  # 测试sr使用
     return 'hello'
